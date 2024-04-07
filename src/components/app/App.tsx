bfa9dc5f8d2c6ffe12c5853fe2406ea4bc815fa7
Yaroslav_Bushmakin <yaroslav_bushmakin@epam.com>
2024-04-07 23:38:44 +0300

    Added routing

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.scss';
 
 import { HomePage } from '../pages/home-page';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 export default function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <div className="App">
+        <HomePage />
+      </div>
+    </Router>
   );
 }
